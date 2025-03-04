d7bcb90c706aa6e233f25f23bf49d58cafefd04d
Mark Truett <mtruett@cissecure.com>
2025-03-04 14:04:13 -0500

    clean slate

---
+++ certs/generate_certs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# generate_certs.sh
+mkdir -p certs
+openssl req -x509 -newkey rsa:4096 -keyout certs/server.key -out certs/server.crt -days 365 -nodes -subj "/CN=localhost"
+chmod 600 certs/server.key
